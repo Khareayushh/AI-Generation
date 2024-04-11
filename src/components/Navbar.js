c4e1db8be2269f161fab99738d7c5aa79033450f
Ayushh Khare <ayushhkhare07092001@gmail.com>
2024-04-11 22:45:42 +0530

    added source code navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,15 +29,25 @@ const Navbar = () => {
   return (
     <div className="head flex items-center justify-center mt-4 max-md:flex max-md:justify-between max-md:mx-8 transition-all duration-150">
       <p className="font-bold text-3xl bg-transparent ">Persona</p>
-      <p
-        className="absolute right-8 hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out max-md:hidden"
-        onClick={(e) => {
-          handleSmoothScroll(e);
-        }}
-        href="#about"
-      >
-        About Persona
-      </p>
+      <div className="flex items-center justify-center gap-4 absolute right-8">
+        <p
+          className=" hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out max-md:hidden"
+          onClick={(e) => {
+            handleSmoothScroll(e);
+          }}
+          href="#about"
+        >
+          About Persona
+        </p>
+        <a href="https://github.com/Khareayushh/fav_giphy">
+          <p
+            className="hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out max-md:hidden"
+            href="#about"
+          >
+            Source Code
+          </p>
+        </a>
+      </div>
       <div className="relative transition-all duration-500">
         {ham ? (
           <div onClick={handleham} className="md:hidden">
@@ -52,17 +62,26 @@ const Navbar = () => {
         )}
       </div>
       {ham && (
-        <div className="absolute bg-slate-300  top-16 right-0 transition-all p-4 max-md:block">
-          <p
-            className="hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out"
-            onClick={(e) => {
-              handleSmoothScroll(e);
-              handleham(); // Close the menu after clicking a link
-            }}
-            href="#about"
-          >
-            About Persona
-          </p>
+        <div className="rounded-lg absolute bg-slate-300  top-16 right-0 transition-all p-4 max-md:block">
+          <div className="flex flex-col">
+            <p
+              className="hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out"
+              onClick={(e) => {
+                handleSmoothScroll(e);
+                handleham(); // Close the menu after clicking a link
+              }}
+              href="#about"
+            >
+              About Persona
+            </p>
+            <a href="https://github.com/Khareayushh/fav_giphy" target="_blank" className="mt-2 border-t-4 pt-2">
+              <p
+                className="hover:text-red-600 cursor-pointer font-bold transition-all duration-300 ease-in-out"
+              >
+                Source Code
+              </p>
+            </a>
+          </div>
           {/* Add more menu items as needed */}
         </div>
       )}
